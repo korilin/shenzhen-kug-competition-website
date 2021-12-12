9e21ec7bed61309bc1677da411a2167634bc93af
korilin <korilin.dev@gmail.com>
2021-12-12 17:31:32 +0800

    添加 Seminar Now 布局

---
+++ src/components/SeminarNow.vue
@@ -0,0 +1,70 @@
+<script setup>
+
+import { ref } from "vue-demi"
+import APIs from "../utils/apis"
+import httpTool from "../utils/http-tool"
+import { dateFromTimestamp } from "../utils/utils"
+
+const svgSize = 18
+const seminarNow = ref(null)
+
+const isEnter = ref(true)
+
+httpTool.get(APIs.seminar_now).then((response) => {
+    seminarNow.value = response.data
+    console.log(seminarNow);
+})
+</script>
+
+<template>
+    <el-empty v-if="seminarNow == null" :image-size="100" description="还没开始"></el-empty>
+
+    <div v-else>
+        <el-descriptions class="margin-top" :column="1" border :title="seminarNow.title">
+            <el-descriptions-item>
+                <template #label>
+                    <div class="el-desc-label">
+                        <timer :width="svgSize" :height="svgSize" />开始时间
+                    </div>
+                </template>
+                {{ dateFromTimestamp(seminarNow.eventTime) }}
+            </el-descriptions-item>
+            <el-descriptions-item>
+                <template #label>
+                    <div class="el-desc-label">
+                        <notebook :width="svgSize" :height="svgSize" />主题
+                    </div>
+                </template>
+                <el-tag
+                    type="primary"
+                    v-for="theme in seminarNow.themes"
+                    style="margin-right: 10px;"
+                >{{ theme }}</el-tag>
+            </el-descriptions-item>
+            <el-descriptions-item>
+                <template #label>
+                    <div class="el-desc-label">
+                        <tickets :width="svgSize" :height="svgSize" />介绍
+                    </div>
+                </template>
+                {{ seminarNow.description }}
+            </el-descriptions-item>
+        </el-descriptions>
+        <div style="margin: 50px auto 80px; text-align: center;">
+            <el-button type="success" :disabled="isEnter">{{ isEnter ? "已报名" : "报名参加本期 Seminar" }}</el-button>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+svg {
+    position: relative;
+    top: 4px;
+    padding-left: 8px;
+    left: -5px;
+}
+
+.el-desc-label {
+    min-width: 120px;
+}
+</style>
--- src/pages/Seminars.vue
+++ src/pages/Seminars.vue
@@ -1,23 +1,17 @@
 <script setup>
-import { reactive, ref } from "vue-demi";
-import APIs from "../lib/apis";
-import { httpTool } from "../lib/utils"
+import { reactive } from "vue-demi";
+import SeminarNow from "../components/SeminarNow.vue";
+import APIs from "../utils/apis";
+import httpTool from "../utils/http-tool"
+import { dateFromTimestamp } from "../utils/utils"
 
 let seminars = reactive([])
-const seminarNow = ref(null)
 
-httpTool.get(APIs.seminar_all, {}).then((response) => {
-    console.log(response);
+httpTool.get(APIs.seminar_record).then((response) => {
     for (const index in response.data) {
         seminars.push(response.data[index])
     };
-    console.log(seminars);
 })
-
-function dateByTimestamp(timestamp) {
-    const date = new Date(timestamp)
-    return date.toLocaleDateString() + " " + date.toLocaleTimeString()
-}
 
 function openNewTab(url) {
     window.open(url)
@@ -27,7 +21,8 @@ function openNewTab(url) {
 <template>
     <div class="wrap">
         <h1>本期 Thematic Seminar 信息</h1>
-        <el-empty v-if="seminarNow == null" :image-size="100" description="还没开始"></el-empty>
+
+        <SeminarNow />
 
         <el-divider>
             <span class="divider-text">往期活动记录</span>
@@ -35,16 +30,22 @@ function openNewTab(url) {
 
         <el-card v-for="seminar in seminars">
             <h2>{{ seminar.title }}</h2>
-            <p class="time">活动开始时间：{{ dateByTimestamp(seminar.eventTime) }}</p>
-            <p class="theme">活动主题：{{ seminar.theme }}</p>
+            <p class="time">活动开始时间：{{ dateFromTimestamp(seminar.eventTime) }}</p>
+            <p class="theme">
+                活动主题：
+                <el-tag v-for="theme in seminar.themes" style="margin-right: 10px;">{{ theme }}</el-tag>
+            </p>
             <p>{{ seminar.description }}</p>
             <p style="text-align: center; margin-top: 40px;">
                 <el-button
                     type="primary"
                     :disabled="seminar.bilibiliUrl == null"
                     @click="openNewTab(seminar.bilibiliUrl)"
-                    round
-                >观看回放视频</el-button>
+                >
+                    <span style="line-height: 25px;">观看回放视频</span>
+
+                    <video-play :width="18" :height="18" />
+                </el-button>
             </p>
         </el-card>
     </div>
@@ -62,6 +63,12 @@ function openNewTab(url) {
     font-size: 1rem;
 }
 
+svg {
+    position: relative;
+    top: 4px;
+    padding-left: 10px;
+}
+
 .el-card {
     margin-top: 40px;
 
@@ -70,7 +77,6 @@ function openNewTab(url) {
     }
 
     .theme {
-        color: var(--el-color-primary);
         font-weight: bold;
     }
 }
