50981bf5dff3e821a48f4e70eb76c57e94ece43b
korilin <korilin.dev@gmail.com>
2022-01-15 18:49:05 +0800

    init commit

---
+++ src/jsMain/kotlin/main.kt
@@ -0,0 +1,10 @@
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.dom.*
+import org.jetbrains.compose.web.renderComposable
+
+fun main() {
+
+}
