f03c120bc3a56c0bc9e32f2dcaca6a4de03f2f20
korilin <korilin.dev@gmail.com>
2022-01-15 21:56:23 +0800

    info card

--- src/jsMain/kotlin/global/KUGInfo.kt
+++ src/jsMain/kotlin/global/KUGInfo.kt
@@ -1,4 +1,12 @@
 package global
 
 object KUGInfo {
+    const val name: String = "深圳 Kotlin User Group"
+    const val date: String = "2021 年"
+    const val description: String = "欢迎所有热爱 Kotlin 的开发者参与到用户组中，" +
+                "每 2~3 个月我们会举行一次研讨会，进行与 Kotlin 相关的主题分享 ，" +
+                "你可以在 QQ 交流群中和用户组其它开发者一起学习、讨论技术、解决问题 \uD83D\uDCDA"
+    val activityTypeWay: Map<String, String> = mapOf(
+        "QQ 群" to "811981478", "QQ 频道" to "通过频道成员的通行证邀请"
+    )
 }
--- src/jsMain/kotlin/view/home/component/InfoCard.kt
+++ src/jsMain/kotlin/view/home/component/InfoCard.kt
@@ -1,8 +1,33 @@
 package view.home.component
 
 import androidx.compose.runtime.Composable
+import global.KUGInfo
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.css.keywords.auto
 import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.Li
+import org.jetbrains.compose.web.dom.Text
+import org.jetbrains.compose.web.dom.Ul
 
 @Composable
 fun InfoCard(kugInfo: KUGInfo) = Div({
+    style {
+        padding(20.px)
+        margin(10.px)
+        borderRadius(20.px)
+        background("#fff")
+    }
+}) {
+    Div({
+        classes( )
+        style { marginBottom(20.px) }
+    }) { Text(kugInfo.description) }
+    Div({}) {
+        Text("活动方式：")
+        Ul {
+            kugInfo.activityTypeWay.forEach {
+                Li { Text("${it.key}：${it.value}") }
+            }
+        }
+    }
 }
